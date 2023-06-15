626cf555dd8729baf124772e2b8cda9f35224e03
Joao Franco <joaogabriel@pm.me>
2023-06-15 15:54:16 -0300

    solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,140 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const SIZES = {
+  small: {
+    borderBottom: 1,
+    fontSize: 14,
+    iconSize: 16,
+    height: 24,
+    "--padding": "4px",
+  },
+  large: {
+    borderBottom: 2,
+    fontSize: 18,
+    iconSize: 24,
+    height: 36,
+    "--padding": "8px",
+  },
 };
+
+const IconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  const styles = SIZES[size];
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <IconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </IconWrapper>
+      <TextInput
+        {...delegated}
+        style={{
+          "--width": width + "px",
+          "--height": styles.height + "px",
+          "--border-thickness": styles.borderBottom + "px",
+          "--fontSize": styles.fontSize / 16 + "rem",
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--fontSize);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  left: 5px;
+  width: var(--size);
+  height: var(--size);
+`;
+
+const InputWrapper = styled.div`
+  position: relative;
+  left: 80px;
+  width: var(--width);
+  height: var(--height);
+  border-bottom: var(--borderBottom) solid ${COLORS.black};
+  display: inline-block;
+  max-width: 90%;
+  color: ${COLORS.gray700};
+
+  &:focus {
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+const PresentationalInput = styled.input`
+  border: none;
+  font-size: var(--fontSize);
+  font-weight: 700;
+  padding: var(--padding);
+  color: inherit;
+
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 41px;
+
+  margin: auto;
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  ${InputWrapper}:hover {
+    font-weight: 700;
+  }
+`;
+
+const Label = styled.label`
+  color: ${COLORS.black};
+  font-size: ${20 / 16}rem;
+  font-weight: 500;
+  position: relative;
+  display: inline-block;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  height: 100%;
+  margin: auto;
+`;
 
 export default IconInput;
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,95 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const SIZES = {
+  small: {
+    "--height": "8px",
+    "--border-radius": "4px",
+    "--padding": "0px",
+    "--width": "370px",
+  },
+  medium: {
+    "--height": "12px",
+    "--border-radius": "4px",
+    "--padding": "0px",
+    "--width": "370px",
+  },
+  large: {
+    "--height": "24px",
+    "--border-radius": "8px",
+    "--padding": "4px",
+    "--width": "370px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  value = value > 100 ? 100 : value;
+  return (
+    <>
+      <LabelProgress for="loadinglabel">{value}%</LabelProgress>
+      <ProgressBarBase
+        id="loadinglabel"
+        max="100"
+        value={value}
+        style={styles}
+      ></ProgressBarBase>
+    </>
+  );
 };
+
+const LabelProgress = styled.label`
+  @import url("https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap");
+  font-family: "Roboto", sans-serif;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  position: absolute;
+  left: 30px;
+  top: 30px;
+`;
+
+const ProgressBarBase = styled.progress`
+  width: var(--width);
+  height: var(--height);
+  position: absolute;
+  left: 150px;
+  top: 30px;
+  margin: auto 0;
+
+  appearance: none;
+
+  &::-webkit-progress-bar {
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    border-radius: var(--border-radius);
+    padding: var(--padding);
+    overflow: hidden;
+  }
+
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: ${(p) => (p.value === 100 ? "4px" : "4px 0px 0px 4px")};
+  }
+
+  &::-moz-progress-bar {
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+    border-radius: var(--border-radius);
+    padding: var(--padding);
+    overflow: hidden;
+    background-color: ${COLORS.primary};
+    border-radius: ${(p) => (p.value === 100 ? "4px" : "4px 0px 0px 4px")};
+  }
+`;
 
 export default ProgressBar;
